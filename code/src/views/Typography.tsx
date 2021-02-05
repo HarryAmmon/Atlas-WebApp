e53fd78c626f69d381227355c48f3464bc1c1d09
HarryAmmon <harry_a99@hotmail.co.uk>
2021-02-05 15:39:37 +0000

    Typography page

--- code/src/views/Typography.tsx
+++ code/src/views/Typography.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Button, Box, Card, Typography } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  Typography,
+  CardHeader,
+  Divider,
+  Box,
+} from "@material-ui/core";
 
 export const TypographyPage = () => (
   <React.Fragment>
@@ -9,17 +16,30 @@ export const TypographyPage = () => (
     <Typography variant="h4">Heading 4</Typography>
     <Typography variant="h5">Heading 5</Typography>
     <Typography variant="h6">Heading 6</Typography>
-    <Typography variant="body1">Paragraph tag - body1</Typography>
-    <Typography variant="body2">Paragraph tag - body 2</Typography>
     <Typography variant="subtitle1">Heading 6 - Subtitle 1</Typography>
     <Typography variant="subtitle2">Heading 6 - Subtitle 2</Typography>
+    <Typography variant="body1">Paragraph tag - body1</Typography>
+    <Typography variant="body2">Paragraph tag - body 2</Typography>
     <Typography variant="caption">Span - Caption</Typography>
-    <Box mt={2}>
+    <Typography variant="button">Span - Button</Typography>
+    <Typography variant="overline">Span - Overline</Typography>
+    <Box display="flex" justifyContent="space-evenly">
       <Card>
+        <CardHeader title="A title" subheader={"My Subheader"} />
         <Button variant="contained" color="primary">
-          <Typography variant="button">This is variant button</Typography>
+          Primary button
         </Button>
       </Card>
+      <Card>
+        <CardHeader title="Second title" subheader={"My Subheader"} />
+        <Button variant="contained" color="primary">
+          Primary button
+        </Button>
+      </Card>
+    </Box>
+
+    <Box my={3}>
+      <Divider />
     </Box>
   </React.Fragment>
 );
