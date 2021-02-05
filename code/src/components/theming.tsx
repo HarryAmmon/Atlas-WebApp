f257dcecf6e7ca56b626c9e652acc7ea7b3b5626
HarryAmmon <harry_a99@hotmail.co.uk>
2021-02-05 15:39:00 +0000

    Use custom theme

---
+++ code/src/components/theming.tsx
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const CustomTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#5490FF",
+      main: "#4C82E6",
+      dark: "#2A4880",
+    },
+  },
+});
--- code/src/index.tsx
+++ code/src/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from "@material-ui/core";
+import { CustomTheme } from "./components/theming";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={CustomTheme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
