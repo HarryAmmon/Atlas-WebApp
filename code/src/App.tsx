2c225874699eb176d5d14c9e40151166cd88d8f8
HarryAmmon <harry_a99@hotmail.co.uk>
2021-02-09 17:36:04 +0000

    Create homepage

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { TypographyPage } from "./views/Typography";
+import { TypographyPage, Home } from "./views";
 import { Route, Switch } from "react-router";
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/" component={() => <Home />} />
         <Route path="/typography" component={() => <TypographyPage />} />
       </Switch>
     </div>
