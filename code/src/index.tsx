e3fddda9fc85589d5a7da072ddb2f137da4aa9f6
HarryAmmon <harry_a99@hotmail.co.uk>
2021-03-29 10:11:03 +0100

    Use azure backend

--- code/src/index.tsx
+++ code/src/index.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 import { ThemeProvider } from "@material-ui/core";
 import { CustomTheme } from "./components/theming";
 
-axios.defaults.baseURL = "https://localhost:5001";
+axios.defaults.baseURL = "https://ci601-api.azurewebsites.net";
 
 ReactDOM.render(
   <ThemeProvider theme={CustomTheme}>
