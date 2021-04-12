f1b333c585c9547dc3cbee1dc4dd3d5417d89b68
HarryAmmon <harry_a99@hotmail.co.uk>
2021-04-12 11:39:06 +0100

    Refactor TaskWithCheckbox

--- code/src/components/cards/Task/Task.tsx
+++ code/src/components/cards/Task/Task.tsx
@@ -1,22 +1,59 @@
-import { Dialog } from "@material-ui/core";
-import React, { useState } from "react";
+import { Box, Checkbox, Dialog } from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useReducer, useState } from "react";
 import { BaseCard } from "../BaseCard/BaseCard";
-import { TaskProps } from "./types";
+import { TaskContext } from "./services/TaskContext";
+import { TaskReducer } from "./services/TaskReducer";
+import { TaskProps, TaskFields } from "./types";
 import { Details } from "./views/Details";
 import { Summary } from "./views/Summary";
+import styles from "./Task.module.scss";
 
 export const Task: React.FC<TaskProps> = ({ id, className }) => {
   const [showDetails, setShowDetails] = useState<boolean>(false);
+  let task: TaskFields = { id: "", title: "", completed: false };
   const handleOpen = () => setShowDetails(true);
   const handleClose = () => setShowDetails(false);
+  const [taskState, taskDispatcher] = useReducer(TaskReducer, task);
+
+  useEffect(() => {
+    axios
+      .get(`Task/${id}`)
+      .then((result) =>
+        taskDispatcher({ type: "ADD_TASK", Task: result.data })
+      );
+  }, [id]);
+
   return (
-    <>
-      <BaseCard variant="Task" className={className} changeView={handleOpen}>
-        <Summary id={id} />
-      </BaseCard>
+    <TaskContext.Provider
+      value={{ Task: taskState, TaskDispatcher: taskDispatcher }}
+    >
+      <Box className={styles.checkboxWithSummary}>
+        <Checkbox
+          onChange={() => {
+            axios
+              .put(`Task/${taskState.id}`, {
+                ...taskState,
+                completed: !taskState.completed,
+              })
+              .then((result) => {
+                console.log("success");
+                taskDispatcher({
+                  type: "SET_COMPLETED",
+                  completed: !taskState.completed,
+                });
+              })
+              .catch((err) => console.log(err));
+          }}
+          checked={taskState.completed}
+        />
+        <BaseCard variant="Task" className={className} changeView={handleOpen}>
+          <Summary />
+        </BaseCard>
+      </Box>
       <Dialog open={showDetails} onClose={handleClose} maxWidth="xl" fullWidth>
-        <Details id={id} showDetails={showDetails} handleClose={handleClose} />
+        <Details showDetails={showDetails} handleClose={handleClose} />
       </Dialog>
-    </>
+    </TaskContext.Provider>
   );
 };
